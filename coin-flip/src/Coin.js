ce26c04ad2f38a2df645e86874a04b8531a29a6d
Joke Heyndels <jo_hannah@outlook.com>
2021-03-19 16:36:11 +0000

    Flipping the actual coin!

--- coin-flip/src/Coin.js
+++ coin-flip/src/Coin.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import heads from "./heads.png"
+import tails from "./tails.png"
 
 class Coin extends Component {
     render(){
-        return <p>I'm {this.props.face}</p>
+        console.log(this.props)
+        return (
+            this.props.face === "heads"
+            ? <img src={heads} alt="Two Euro coin: heads - Ireland's harp" />
+            : <img src={tails} alt="Two Euro coin: tails"/>
+        )
     }
 }
 
